edfe20a70b0a94d5afa1f188554bbea2654e527d
Sandro Mijatovic <mija.sand@gmail.com>
2017-03-02 10:23:54 -0800

    Add success page when recipe added

--- HomeModule/HomeModule.cs
+++ HomeModule/HomeModule.cs
@@ -29,7 +29,25 @@ namespace Cookbook
                 List<Category> AllCategories = Category.GetAll();
                 return View["categories.cshtml", AllCategories];
             };
-            //Shows all recipes under that particular category
+            //Finds the proper category, and lists the recipes within it. Recipes can be added to this category from here.
+            Get["/categories/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              Category SelectedCategory = Category.Find(parameters.id);
+              List<Recipe> CategoryRecipes = SelectedCategory.GetRecipes();
+              List<Recipe> AllRecipes = Recipe.GetAll();
+              model.Add("category", SelectedCategory);
+              model.Add("categoryRecipes", CategoryRecipes);
+              model.Add("AllRecipes", AllRecipes);
+              return View["category.cshtml", model];
+            };
+            //Add a recipe to a category
+            Post["/recipe_added"] = _ => {
+              Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
+              Category category = Category.Find(Request.Form["category-id"]);
+              category.AddRecipe(recipe);
+              return View["success.cshtml"];
+            };
+            //Shows all recipes under that particular category; Gives you option to add a recipe
             Get["/recipes"] = _ => {
                 List<Recipe> AllRecipes = Recipe.GetAll();
                 return View["recipes.cshtml", AllRecipes];
@@ -46,6 +64,17 @@ namespace Cookbook
                 newRecipe.Save();
                 List<Recipe> AllRecipes = Recipe.GetAll();
                 return View["recipes.cshtml", AllRecipes];
+            };
+            //Find targeted recipe and all associated categories. Recipe can be added to category.
+            Get["/recipes/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              Recipe SelectedRecipe = Recipe.Find(parameters.id);
+              List<Category> RecipeCategories = SelectedRecipe.GetCategories();
+              List<Category> AllCategories = Category.GetAll();
+              model.Add("recipe", SelectedRecipe);
+              model.Add("recipeCategories", RecipeCategories);
+              model.Add("allCategories", AllCategories);
+              return View["recipe.cshtml", model];
             };
         }
     }
