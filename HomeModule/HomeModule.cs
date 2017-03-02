097e77583947b14452728c8ee65b4c02177f0d62
grinil khanna and sandro mijatovic <grinil+sandro@>
2017-03-01 16:46:31 -0800

    Add notes to home module regarding description of pages.

--- HomeModule/HomeModule.cs
+++ HomeModule/HomeModule.cs
@@ -8,31 +8,39 @@ namespace Cookbook
     {
         public HomeModule()
         {
+            //Shows index page
             Get["/"] = _ => {
                 return View["index.cshtml"];
             };
+            //Shows all styles of food recipes
             Get["/categories"] = _ => {
                 List<Category> AllCategories = Category.GetAll();
                 return View["categories.cshtml", AllCategories];
             };
+
+            // Displays form to input new category
             Get["/categories/new"] = _ => {
                 return View["category_new.cshtml"];
             };
+            // Demonstrates that the add was successful
             Post["/categories/new"] = _ => {
                 Category newCategory = new Category(Request.Form["category-name"]);
                 newCategory.Save();
                 List<Category> AllCategories = Category.GetAll();
                 return View["categories.cshtml", AllCategories];
             };
+            //Shows all recipes under that particular category
             Get["/recipes"] = _ => {
                 List<Recipe> AllRecipes = Recipe.GetAll();
                 return View["recipes.cshtml", AllRecipes];
             };
 
+            // Displays the form to add a new recipe
             Get["/recipes/new"] = _ => {
                 return View["recipe_new.cshtml"];
             };
 
+            // Demonstrates that the add for recipe was successful
             Post["/recipes/new"] = _ => {
                 Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredients"], Request.Form["recipe-instructions"], Request.Form["recipe-rating"]);
                 newRecipe.Save();
