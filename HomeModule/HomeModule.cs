57519b479a2dedadd22ff9b25710e3bccc80eea6
grinil khanna and sandro mijatovic <grinil+sandro@>
2017-03-01 16:39:04 -0800

    add form pages for class objs and allow them to display

--- HomeModule/HomeModule.cs
+++ HomeModule/HomeModule.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
 
-namespace NAME
+namespace Cookbook
 {
     public class HomeModule : NancyModule
     {
@@ -10,6 +10,34 @@ namespace NAME
         {
             Get["/"] = _ => {
                 return View["index.cshtml"];
+            };
+            Get["/categories"] = _ => {
+                List<Category> AllCategories = Category.GetAll();
+                return View["categories.cshtml", AllCategories];
+            };
+            Get["/categories/new"] = _ => {
+                return View["category_new.cshtml"];
+            };
+            Post["/categories/new"] = _ => {
+                Category newCategory = new Category(Request.Form["category-name"]);
+                newCategory.Save();
+                List<Category> AllCategories = Category.GetAll();
+                return View["categories.cshtml", AllCategories];
+            };
+            Get["/recipes"] = _ => {
+                List<Recipe> AllRecipes = Recipe.GetAll();
+                return View["recipes.cshtml", AllRecipes];
+            };
+
+            Get["/recipes/new"] = _ => {
+                return View["recipe_new.cshtml"];
+            };
+
+            Post["/recipes/new"] = _ => {
+                Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredients"], Request.Form["recipe-instructions"], Request.Form["recipe-rating"]);
+                newRecipe.Save();
+                List<Recipe> AllRecipes = Recipe.GetAll();
+                return View["recipes.cshtml", AllRecipes];
             };
         }
     }
